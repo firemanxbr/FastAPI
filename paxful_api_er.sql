b4dd3f372ff29d0ff3b4e77bdc1d8ad53acc032c
Marcelo Barbosa <firemanxbr@fedoraproject.org>
2019-09-05 22:51:18 +0300

    updated SQL base to update maximium wallets allowed per user

--- paxful_api_er.sql
+++ paxful_api_er.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS users
     name VARCHAR(255),
     email VARCHAR(255) NOT NULL UNIQUE,
     token VARCHAR(255) NOT NULL UNIQUE,
-    max_wallet TINYINT DEFAULT(10),
+    max_wallet TINYINT UNSIGNED DEFAULT(10),
     date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)
     ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS wallets
     ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS transactions
-    (transaction_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    (transaction_id INT AUTO_INCREMENT PRIMARY KEY,
     address_income VARCHAR(255) NOT NULL,
     address_outcome VARCHAR(255) NOT NULL,
     amount DECIMAL(10,8) NOT NULL,
@@ -32,6 +32,9 @@ CREATE TABLE IF NOT EXISTS transactions
 
 CREATE TRIGGER update_user_wallet_balance BEFORE INSERT ON transactions FOR EACH ROW
 	UPDATE wallets SET balance = balance - NEW.amount WHERE address = NEW.address_outcome;
+    
+CREATE TRIGGER update_user_wallet_limit BEFORE INSERT ON wallets FOR EACH ROW
+	UPDATE users SET max_wallet = max_wallet - 1 WHERE user_id = NEW.user_id;
 
 INSERT INTO users(name, email, token) VALUES("User 1", "user_1@gmail.com", "86f41a39c3a243fd22d96228eaeb23a60df36e76");
 INSERT INTO users(name, email, token) VALUES("User 2", "user_2@gmail.com", "75z32b48c3a243fd22d96228eaeb23a60df36e75");
