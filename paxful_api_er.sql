6f8d9ade41481d38ff442598a976b5cc19ca30ed
Marcelo Barbosa <firemanxbr@fedoraproject.org>
2019-09-05 22:32:35 +0300

    updated SQL base

--- paxful_api_er.sql
+++ paxful_api_er.sql
@@ -3,34 +3,38 @@ CREATE DATABASE IF NOT EXISTS paxful;
 USE paxful;
 
 CREATE TABLE IF NOT EXISTS users
-    (user_id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    (user_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     email VARCHAR(255) NOT NULL UNIQUE,
     token VARCHAR(255) NOT NULL UNIQUE,
-    max_wallet TINYINT(4) DEFAULT(10),
-    date_created DATETIME NOT NULL)
-    ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    max_wallet TINYINT DEFAULT(10),
+    date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)
+    ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS wallets
-    (wallet_id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    (wallet_id INT AUTO_INCREMENT PRIMARY KEY,
     address VARCHAR(255) NOT NULL UNIQUE,
     balance DECIMAL(10,8) DEFAULT(1),
-    user_id INT(11),
-    date_created DATETIME NOT NULL,
-    CONSTRAINT fk_wallets_users FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE RESTRICT ON UPDATE CASCADE)
-    ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    user_id INT,
+    date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_wallets_users FOREIGN KEY (user_id) REFERENCES users(user_id))
+    ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS transactions
-    (transaction_id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    (transaction_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     address_income VARCHAR(255) NOT NULL,
     address_outcome VARCHAR(255) NOT NULL,
-    amount DECIMAL(10,8) NOT NULL
-    user_id INT(11),
-    date_created DATETIME NOT NULL,
-    CONSTRAINT fk_transactions_users FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE RESTRICT ON UPDATE CASCADE)
-    ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    amount DECIMAL(10,8) NOT NULL,
+    user_id INT,
+    date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_transactions_users FOREIGN KEY (user_id) REFERENCES users(user_id))
+    ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/* Test 
-INSERT INTO paxful.users(name, email, token) values("Marcelo Barbosa", "mr.marcelo.barbosa@gmail.com", "86f41a39c3a243fd22d96228eaeb23a60df36e76");
-INSERT INTO paxful.wallets(address, user_id) values("bc1q84y2quplejutvu0h4gw9hy59fppu3thg0u2xz3", "1");
-*/
+CREATE TRIGGER update_user_wallet_balance BEFORE INSERT ON transactions FOR EACH ROW
+	UPDATE wallets SET balance = balance - NEW.amount WHERE address = NEW.address_outcome;
+
+INSERT INTO users(name, email, token) VALUES("User 1", "user_1@gmail.com", "86f41a39c3a243fd22d96228eaeb23a60df36e76");
+INSERT INTO users(name, email, token) VALUES("User 2", "user_2@gmail.com", "75z32b48c3a243fd22d96228eaeb23a60df36e75");
+INSERT INTO wallets(address, user_id) VALUES("bc1q84y2quplejutvu0h4gw9hy59fppu3thg0u2xz3", "1");
+INSERT INTO wallets(address, user_id) VALUES("ab2m73q1mzojdbytvu0h4gw9hy59fppu3thg0u1ny2", "2");
+INSERT INTO transactions(address_income, address_outcome, amount, user_id) VALUES("ab2m73q1mzojdbytvu0h4gw9hy59fppu3thg0u1ny2", "bc1q84y2quplejutvu0h4gw9hy59fppu3thg0u2xz3", "0.5", "1");
